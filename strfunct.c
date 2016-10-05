11a7d5072a9b5c9c437fb61d4a3196d3b583e531
adit-nicholas <adit.nicholas@gmail.com>
2016-10-05 10:58:40 -0400

    Update strfunct.c

--- strfunct.c
+++ strfunct.c
@@ -18,6 +18,41 @@ char * nstrcpy(char *here, char *move){
   return here;
 }
 
+char * nstrncat(char *here, char *move, size_t n){
+  int x;
+  int y = strlen(here);
+  for (x = 0 ; x < n; x++){
+    here[y + x] = move[x];
+  }
+  return here;
+}
+
+int nstrcmp(char *s1, char *s2){
+  int x;
+  int y;
+  if (strlen(s1) < strlen(s2))
+    y = strlen(s1);
+  else
+    y = strlen(s2);
+  for (x = 0; x < y; x++){
+    if(s1[x] < s2[x] || s2[x] < s1[x]){
+      return s1[x] - s2[x];
+    }
+  }
+  return 0;
+}
+
+char *nstrchr( char *s, char c){
+  int x ;
+  for(x = 0; x < strlen(s); x++){
+    if (s[x] == c){
+      return s[x];
+    }
+  }
+  return 0;
+}
+  
+
 int main(){
   char y[] = "string";
   char x[] = "int";
@@ -27,5 +62,16 @@ int main(){
   printf("Before: %s\n", y);
   printf("Points to %d\n", strcpy(y,x));
   printf("Now: %s\n", y);
-  
+  char a[] = "string";
+  char b[] = "int";
+  printf("Before : %s \n", a);
+  printf("After custom cat: %s \n", nstrncat(a,b,2));
+  printf("B: %s \n", b);
+  printf("Custom Compare: %d\n", nstrcmp(x,b));
+  printf("Regular Compare: %d\n",strcmp(x,b));
+  char h = "t";
+  printf("Custom char finder: %d\n", nstrchr(a,h));
+  printf("Regular char finder: %d\n", strchr(a,h));
+  return 0;
 }
+  
